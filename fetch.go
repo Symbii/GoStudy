ed9a4fc75e1c15efe65f4a7a7af4c0f18e2c7ff9
Symbii <moon19902009@live.cn>
2018-06-03 22:47:25 +0800

    update fetch version1.1

--- fetch.go
+++ fetch.go
@@ -2,32 +2,45 @@ package main
 
 import (
 	"os"
-	"net/http"
-	"fmt"
-//	"io/ioutil"
-	"io"
 	"strings"
+	"fmt"
+	"net/http"
+	"time"
+	"io"
+	"io/ioutil"
 )
 
 func main(){
+	ch := make(chan string)
+
+
 	for _, url := range os.Args[1:] {
 		b := strings.HasPrefix(url, "https://")
 		if b != true{
 			url = "https://" + url
 		}
+		go fetch(url, ch)
+	}
 
-		resp, err := http.Get(url)
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
-			os.Exit(1)
-		}
-		_, err = io.Copy(os.Stdout, resp.Body)
-		resp.Body.Close()
-		if err != nil{
-			fmt.Fprintf(os.Stderr, "fetch:  reading %s: %v\n", url, err)
-			os.Exit(1)
-		}
-		fmt.Printf("%s", os.Stdout)
+	for range os.Args[1:] {
+		fmt.Println(<-ch)
 	}
 }
 
+func fetch(url string, ch  chan<- string){
+	start := time.Now()
+	resp, err := http.Get(url)
+	if err != nil {
+		ch<- fmt.Sprintf("while getting %s, %v ", url , err)
+		return
+	}
+	nbytes, err:= io.Copy(ioutil.Discard, resp.Body)
+	resp.Body.Close()
+	if err != nil {
+		ch <- fmt.Sprintf("while reading %s,%v", url, err)
+		return
+	}
+	cost := time.Since(start).Seconds()
+	ch<- fmt.Sprintf("Time: %v,\t%v,\t%s",cost,nbytes,url)
+	return
+}
